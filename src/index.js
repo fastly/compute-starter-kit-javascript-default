d001de9b5ac8c35426d293ea2115be0dd4e26420
Leon Brocard <lbrocard@fastly.com>
2021-09-08 11:40:31 +0100

    Use const instead of let
    This change updates assignments from let to const when the variable is
    never reassigned.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ async function handleRequest(event) {
   // logger.log("log message");
 
   // Get the client request from the event
-  let req = event.request;
+  const req = event.request;
 
   // Make any desired changes to the client request.
   req.headers.set("Host", "example.com");
@@ -22,7 +22,7 @@ async function handleRequest(event) {
   // We can filter requests that have unexpected methods.
   const VALID_METHODS = ["GET"];
   if (!VALID_METHODS.includes(req.method)) {
-    let response = new Response("This method is not allowed", {
+    const response = new Response("This method is not allowed", {
       status: 405
     });
 
@@ -30,14 +30,14 @@ async function handleRequest(event) {
     return response;
   }
 
-  let method = req.method;
-  let url = new URL(event.request.url);
+  const method = req.method;
+  const url = new URL(event.request.url);
 
   // If request is a `GET` to the `/` path, send a default response.
   if (method == "GET" && url.pathname == "/") {
-    let headers = new Headers();
+    const headers = new Headers();
     headers.set('Content-Type', 'text/html; charset=utf-8');
-    let response = new Response("<iframe src='https://developer.fastly.com/compute-welcome' style='border:0; position: absolute; top: 0; left: 0; width: 100%; height: 100%'></iframe>\n", {
+    const response = new Response("<iframe src='https://developer.fastly.com/compute-welcome' style='border:0; position: absolute; top: 0; left: 0; width: 100%; height: 100%'></iframe>\n", {
       status: 200,
       headers
     });
@@ -47,7 +47,7 @@ async function handleRequest(event) {
   }
 
   // Catch all other requests and return a 404.
-  let response = new Response("The page you requested could not be found", {
+  const response = new Response("The page you requested could not be found", {
     status: 404
   });
 
