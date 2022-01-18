1cf793677b10fe9272e3d693d52937dbc51afe3b
Valerie Bailey <v@valbailey.com>
2022-01-18 22:36:25 +0000

    Migrate to TypeScript

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,9 @@ const path = require("path");
 const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: "./src/index.ts",
   optimization: {
-    minimize: true
+    minimize: true,
   },
   target: "webworker",
   output: {
@@ -13,20 +13,28 @@ module.exports = {
     libraryTarget: "this",
   },
   module: {
-    // Asset modules are modules that allow the use asset files (fonts, icons, etc) 
+    // Asset modules are modules that allow the use asset files (fonts, icons, etc)
     // without additional configuration or dependencies.
     rules: [
-      // asset/source exports the source code of the asset. 
+      // asset/source exports the source code of the asset.
       // Usage: e.g., import notFoundPage from "./page_404.html"
       {
         test: /\.(txt|html)/,
         type: "asset/source",
       },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
     ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
   },
   plugins: [
     // Polyfills go here.
-    // Used for, e.g., any cross-platform WHATWG, 
+    // Used for, e.g., any cross-platform WHATWG,
     // or core nodejs modules needed for your application.
     new webpack.ProvidePlugin({
       URL: "core-js/web/url",
