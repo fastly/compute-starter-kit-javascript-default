fb26c3d445e922b2acfcf486927d5c6541b0bfef
Katsuyuki Omuro <komuro@fastly.com>
2022-12-01 14:47:18 +0900

    Group comments in webpack.config a little better

--- webpack.config.js
+++ webpack.config.js
@@ -13,12 +13,12 @@ module.exports = {
     libraryTarget: "this",
   },
   module: {
-    // Asset modules are modules that allow the use asset files (fonts, icons, etc) 
-    // without additional configuration or dependencies.
     rules: [
-      // asset/source exports the source code of the asset. 
-      // Usage: e.g., import notFoundPage from "./page_404.html"
       {
+        // Asset modules are modules that allow the use asset files (fonts, icons, etc)
+        // without additional configuration or dependencies.
+        // asset/source exports the source code of the asset.
+        // Usage: e.g., import notFoundPage from "./page_404.html"
         test: /\.(txt|html)/,
         type: "asset/source",
       },
@@ -26,7 +26,9 @@ module.exports = {
   },
   plugins: [
     // Polyfills go here.
-    // Used for, e.g., any cross-platform WHATWG, 
+    // Used for, e.g., any cross-platform WHATWG,
     // or core nodejs modules needed for your application.
+    // new webpack.ProvidePlugin({
+    // }),
   ],
 };
