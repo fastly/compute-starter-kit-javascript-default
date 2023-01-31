b797b54dcd0d9255f570a4666a99466d7f958932
Dora Militaru <dora@kalneus.com>
2023-01-31 12:52:43 +0000

    reinstate webpack support

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./src/index.js",
+  optimization: {
+    minimize: true
+  },
+  target: "webworker",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "bin"),
+    libraryTarget: "this",
+  },
+  module: {
+    rules: [
+      // Loaders go here.
+      // e.g., ts-loader for TypeScript
+    ],
+  },
+  plugins: [
+    // Polyfills go here.
+    // Used for, e.g., any cross-platform WHATWG,
+    // or core nodejs modules needed for your application.
+    // new webpack.ProvidePlugin({
+    // }),
+  ],
+  externals: [
+    ({request,}, callback) => {
+      // Allow Webpack to handle fastly:* namespaced module imports by treating
+      // them as modules rather than try to process them as URLs
+      if (/^fastly:.*$/.test(request)) {
+        return callback(null, 'commonjs ' + request);
+      }
+      callback();
+    }
+  ],
+};
