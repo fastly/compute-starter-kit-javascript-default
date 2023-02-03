e56eefb7a96850f4be98a9723a6b168ac7ca8212
Dora Militaru <dora@kalneus.com>
2023-02-03 17:25:37 +0000

    no type module

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-import * as path from "path";
-import * as webpack from "webpack";
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
